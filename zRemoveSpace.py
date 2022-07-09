fe0bf329e9d7e4b80d7db023b55eefe7e6918613
Armour12 <pateldhrna17ce@student.mes.ac.in>
2022-07-10 00:29:16 +0530

    for file name

---
+++ zRemoveSpace.py
@@ -0,0 +1,8 @@
+
+# Python3 code to remove whitespace
+def remove(string):
+    return string.replace(" ", "")
+     
+# Driver Program
+string = 'First and Last Position of an Element In Sorted Array'
+print(remove(string))

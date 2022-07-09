24fc9d595e07ca3b23502f384caf69fb8727219a
Armour12 <pateldhrna17ce@student.mes.ac.in>
2022-07-09 21:32:26 +0530

    added

--- sort01.cpp
+++ sort01.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 int main(){
-    int i=0,j=10;
+    int i=0,j=9;
     int arr[10]={0,1,1,0,0,1,1,0,1,0};
     while(i<j){
-        if(arr[i]==0)
+        while(arr[i]==0)
         {
             i++;
         }
-        else if(arr[j]==1){
+        while(arr[j]==1){
             j--;
         }
-        else if(arr[i]==1 && arr[j]==0)
-        {   
+        
             int temp=arr[i];
             arr[i]=arr[j];
             arr[j]=temp;
             i++;
             j--;
-        }
+                                                                                
 
     }
     for(int p=0;p<10;p++)
